b49e58b8b3849f06c41b554fd2920a4742450faa
Emma Mattner <emattner1997@gmail.com>
2021-08-24 15:44:02 +0930

    queryselect elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const userFormEl = document.querySelector('#user-form');
+const cityInputEl = document.querySelector('#city-input');
+const cityContainer = document.querySelector('#city-container');
+const citySearchTerm = document.querySelector('#city-search-term');
